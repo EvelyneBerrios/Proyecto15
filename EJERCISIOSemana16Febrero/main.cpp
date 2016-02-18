b411e18310030c1b2ec65e26fdb4b6deea1bbd36
EvelyneBerrios <evelyne.berrios@hotmail.com>
2016-02-18 17:54:05 -0600

    Ejercisio del 16 de febrero

---
+++ EJERCISIOSemana16Febrero/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+/*
+Ingresar el nombre, las horas y el turno
+usando un procedimiento.
+luego usando una funcion devolver el pago por hora
+de acuerdo al turno turno
+1    100
+2    120
+3    150
+elaborar un procedimiento para calcular el pago
+de horas trabajadas del empleado y presentarlo
+
+*/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
